394738268c9ada35d484647d7d4330c6036687da
sethmcfarlane <43263474+sethmcfarlane@users.noreply.github.com>
2018-10-04 23:46:12 -0700

    Update colored-lines.js

--- colored-lines.js
+++ colored-lines.js
@@ -5,7 +5,7 @@
     function draw() {
         background(255);
 
-        for (var x = 10; x < width-10; x = x + 40) {
+        for (var x = 10; x < width-10; x = x + 10) {
             colorMode(HSB);
             stroke(x, 80, 80);
             line(x, height/2, mouseX, mouseY);
