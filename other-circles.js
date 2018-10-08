cbd8e5bc7c7beacad7767808b48458ccd304a5b5
sethmcfarlane <43263474+sethmcfarlane@users.noreply.github.com>
2018-10-08 12:33:08 -0700

    Update other-circles.js

--- other-circles.js
+++ other-circles.js
@@ -10,14 +10,15 @@
         noFill();
 
         x.push(mouseX); // equivalent to append(x, mouseX)
-        y.push(mouseY + 30); // equivalent to append(y, mouseY)
+        y.push(mouseY); // equivalent to append(y, mouseY)
 
         for (var i = 0; i < x.length; i = i + 10) {
-            triangle(x[i], y[i], x[i] - 20, y[i] - 50, x[i] + 20, y[i] - 50);
+            // triangle(x[i], y[i], x[i] - 20, y[i] - 50, x[i] + 20, y[i] - 50);
+					triangle(x[i], y[i] + (i + 10), x[i] - (i + 10), y[i] - (i + 10), x[i] + (i + 10), y[i] - (i + 10));
 
         }
 
-        x = x.slice(-100); // keep the last 50 x values
-        y = y.slice(-100); // keep the last 50 y values
+        x = x.slice(-50); // keep the last 50 x values
+        y = y.slice(-50); // keep the last 50 y values
 
     }
